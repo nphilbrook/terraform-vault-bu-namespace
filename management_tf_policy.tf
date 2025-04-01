d0f43ced17b2dec3abfa85fc66aacae1565e3642
Nick Philbrook <42616679+nphilbrook@users.noreply.github.com>
2025-04-01 08:06:40 -0400

    add JWT role permissions

--- management_tf_policy.tf
+++ management_tf_policy.tf
@@ -1,4 +1,6 @@
 data "vault_policy_document" "management" {
+
+
   # ======= AWS secrets engine setup =========
   rule {
     path         = "sys/mounts/aws"
@@ -22,6 +24,12 @@ data "vault_policy_document" "management" {
     path         = "sys/policies/acl/aws-*"
     capabilities = ["create", "read", "update", "list", "delete"]
     description  = "manage policies for dynamic AWS credentials (prefix by convention)"
+  }
+
+  rule {
+    path         = "auth/jwt/role/hcp-tf*"
+    capabilities = ["create", "read", "update", "list", "delete"]
+    description  = "manage JWT auth for TF roles"
   }
   # ======= End AWS secrets engine setup =========
 }
