4c3c03c780cf65eee701ddaad513655eae548f7f
Nick Philbrook <42616679+nphilbrook@users.noreply.github.com>
2025-04-11 14:20:42 -0400

    Fix AWS creation logic and add tests

--- aws.tf
+++ aws.tf
@@ -1,4 +1,6 @@
 resource "vault_aws_secret_backend" "aws" {
+  count = var.configure_aws ? 1 : 0
+
   namespace  = vault_namespace.this.path
   access_key = var.initial_aws_access_key_id
   secret_key = var.initial_aws_secret_access_key
@@ -11,25 +13,31 @@ resource "vault_aws_secret_backend" "aws" {
 }
 
 resource "vault_aws_secret_backend_role" "vault_aws_role" {
+  count = var.configure_aws ? 1 : 0
+
   namespace       = vault_namespace.this.path
-  backend         = vault_aws_secret_backend.aws.path
+  backend         = vault_aws_secret_backend.aws[0].path
   name            = "probable-pancake-tf"
   credential_type = "assumed_role"
   role_arns       = ["arn:aws:iam::517068637116:role/dyn-ec2-access", "arn:aws:iam::517068637116:role/s3-full-access"]
 }
 
 resource "vault_policy" "probable_pancake" {
+  count = var.configure_aws ? 1 : 0
+
   namespace = vault_namespace.this.path
   name      = "aws-probable-pancake"
   # ref below
-  policy = data.vault_policy_document.probable_pancake.hcl
+  policy = data.vault_policy_document.probable_pancake[0].hcl
 }
 
 resource "vault_jwt_auth_backend_role" "probable_pancake" {
+  count = var.configure_aws ? 1 : 0
+
   namespace      = vault_namespace.this.path
   backend        = vault_jwt_auth_backend.jwt_hcp_tf.path
   role_name      = "hcp-tf-probable-pancake"
-  token_policies = ["default", vault_policy.probable_pancake.name]
+  token_policies = ["default", vault_policy.probable_pancake[0].name]
 
   bound_audiences = ["vault.workload.identity"]
   bound_claims = {
@@ -41,14 +49,16 @@ resource "vault_jwt_auth_backend_role" "probable_pancake" {
 }
 
 data "vault_policy_document" "probable_pancake" {
+  count = var.configure_aws ? 1 : 0
+
   rule {
-    path         = "aws/creds/${vault_aws_secret_backend_role.vault_aws_role.name}"
+    path         = "aws/creds/${vault_aws_secret_backend_role.vault_aws_role[0].name}"
     capabilities = ["read"]
     description  = "Read dynamic AWS credentials for the specified role"
   }
 
   rule {
-    path         = "aws/sts/${vault_aws_secret_backend_role.vault_aws_role.name}"
+    path         = "aws/sts/${vault_aws_secret_backend_role.vault_aws_role[0].name}"
     capabilities = ["read", "update", "create"]
     description  = "Read dynamic AWS credentials for the specified role with the STS path."
   }
---
+++ tests/aws.tftest.hcl
@@ -0,0 +1,45 @@
+variables {
+  auth_mount_accessor = "auth_saml_1234abcde"
+  name                = "foobar"
+}
+
+
+run "aws_fail_without_creds" {
+  command = plan
+
+  variables {
+    configure_aws = true
+  }
+
+  expect_failures = [
+    var.initial_aws_access_key_id,
+    var.initial_aws_secret_access_key
+  ]
+}
+
+run "aws_false_success" {
+  command = plan
+
+  variables {
+    configure_aws = false
+  }
+
+  assert {
+    condition     = length(vault_aws_secret_backend.aws) == 0
+    error_message = "AWS Secret Backend created when not expected"
+  }
+}
+
+run "aws_true_success" {
+  command = plan
+
+  variables {
+    configure_aws                 = true
+    initial_aws_access_key_id     = "AKIAXQY5RIOOOOOO"
+    initial_aws_secret_access_key = "aoeu1234"
+  }
+  assert {
+    condition     = length(vault_aws_secret_backend.aws) == 1
+    error_message = "AWS Secret Backend not created when expected"
+  }
+}
