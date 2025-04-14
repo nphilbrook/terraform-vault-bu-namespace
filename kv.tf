7cac840a3ff8556fe5f137c1705fb44227018523
Nick Philbrook <42616679+nphilbrook@users.noreply.github.com>
2025-04-14 16:00:59 -0400

    moar policies

---
+++ kv.tf
@@ -0,0 +1,60 @@
+# Default k/v mounts
+resource "vault_mount" "kv_nonprod" {
+  namespace = vault_namespace.this.path
+  path      = "nonprod/kv"
+  type      = "kv-v2"
+  options = {
+    version = "2"
+    type    = "kv-v2"
+  }
+  description = "nonprod K/V secrets"
+}
+
+resource "vault_mount" "kv_prod" {
+  namespace = vault_namespace.this.path
+  path      = "prod/kv"
+  type      = "kv-v2"
+  options = {
+    version = "2"
+    type    = "kv-v2"
+  }
+  description = "prod K/V secrets"
+}
+
+resource "vault_policy" "kv_nonprod" {
+  name = "kv-nonprod"
+  # ref kv_policies.tf
+  policy = data.vault_policy_document.kv_nonprod.hcl
+}
+
+resource "vault_policy" "kv_prod" {
+  name = "kv-prod"
+  # ref kv_policies.tf
+  policy = data.vault_policy_document.kv_prod.hcl
+}
+
+# External group for nonprod
+resource "vault_identity_group" "kv_nonprod" {
+  name     = "kv_nonprod"
+  type     = "external"
+  policies = [vault_policy.kv_nonprod.name]
+}
+
+resource "vault_identity_group_alias" "kv_nonprod" {
+  name           = "sg_${vault_namespace.this.path}_appdev_nonprod"
+  mount_accessor = var.auth_mount_accessor
+  canonical_id   = vault_identity_group.kv_nonprod.id
+}
+
+# External group for prod
+resource "vault_identity_group" "kv_prod" {
+  name     = "kv_nonprod"
+  type     = "external"
+  policies = [vault_policy.kv_nonprod.name, vault_policy.kv_prod.name]
+}
+
+resource "vault_identity_group_alias" "kv_prod" {
+  name           = "sg_${vault_namespace.this.path}_appdev_prod"
+  mount_accessor = var.auth_mount_accessor
+  canonical_id   = vault_identity_group.kv_prod.id
+}
--- kv_policies.tf
+++ kv_policies.tf
@@ -1,7 +1,15 @@
-data "vault_policy_document" "kv_nonprod_read" {
+data "vault_policy_document" "kv_nonprod" {
   rule {
-    path         = "${vault_mount.kv_nonprod.path}/???"
-    capabilities = ["read"]
-    description  = "Read kv secrets from nonprod"
+    path         = "${vault_namespace.this.path}/${vault_mount.kv_nonprod.path}*"
+    capabilities = ["create", "update", "patch", "read", "delete", "list"]
+    description  = "manage kv secrets in nonprod"
+  }
+}
+
+data "vault_policy_document" "kv_prod" {
+  rule {
+    path         = "${vault_namespace.this.path}/${vault_mount.kv_prod.path}*"
+    capabilities = ["create", "update", "patch", "read", "delete", "list"]
+    description  = "manage kv secrets in prod"
   }
 }
