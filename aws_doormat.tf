6940ae28f79941e9108b11a4faa5e3ba02847b80
Nick Philbrook <42616679+nphilbrook@users.noreply.github.com>
2025-05-28 15:48:49 -0400

    Remove keys

--- aws_doormat.tf
+++ aws_doormat.tf
@@ -52,21 +52,31 @@ resource "aws_iam_user_policy_attachment" "vault_mount_user" {
   policy_arn = aws_iam_policy.vault_mount_user_policy[0].arn
 }
 
-resource "aws_iam_access_key" "vault_mount_user" {
-  count = var.configure_aws ? 1 : 0
-  user  = aws_iam_user.vault_mount_user[0].name
+# resource "aws_iam_access_key" "vault_mount_user" {
+#   count = var.configure_aws ? 1 : 0
+#   user  = aws_iam_user.vault_mount_user[0].name
+#   lifecycle {
+#     ignore_changes = all
+#   }
+# }
+
+removed {
+  from = aws_iam_access_key.vault_mount_user
+
   lifecycle {
-    ignore_changes = all
+    destroy = false
   }
 }
 
 resource "vault_aws_secret_backend" "aws_doormat" {
   count = var.configure_aws ? 1 : 0
 
-  path       = "aws-doormat"
-  namespace  = vault_namespace.this.path
-  access_key = aws_iam_access_key.vault_mount_user[0].id
-  secret_key = aws_iam_access_key.vault_mount_user[0].secret
+  path      = "aws-doormat"
+  namespace = vault_namespace.this.path
+  # This code will no longer work for net-new invocations of this module, but I'm
+  # ripping all of this stuff out anyway :shrug:
+  access_key = "foo"
+  secret_key = "bar"
   lifecycle {
     ignore_changes = [
       access_key,
