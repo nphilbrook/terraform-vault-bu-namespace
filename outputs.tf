3c1ff1880b899e5c8d2e719dea48edf7f2abb7f2
Nick Philbrook <42616679+nphilbrook@users.noreply.github.com>
2025-05-29 16:24:26 -0400

    Add an output for the path

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,8 @@ output "namespace_id" {
   description = "The internal ID Vault assigns this namespace."
   value       = vault_namespace.this.namespace_id
 }
+
+output "path" {
+  description = "The path to the namespace."
+  value       = vault_namespace.this.path
+}
